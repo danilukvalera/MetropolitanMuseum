8354a566ed5ce40d6b3e9bccb5d8064ad5b91dcf
danilukvalera <danilukvalera@ukr.net>
2021-03-11 16:57:39 +0200

    11.03.21. работа

--- app/src/main/java/com/daniluk/metropolitanmuseum/ShowpieceActivity.kt
+++ app/src/main/java/com/daniluk/metropolitanmuseum/ShowpieceActivity.kt
@@ -2,14 +2,27 @@ package com.daniluk.metropolitanmuseum
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ViewSwitcher
+import androidx.appcompat.app.AppCompatActivity
 import com.daniluk.metropolitanmuseum.pojo.Showpiece
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_showpiece.*
 
-class ShowpieceActivity : AppCompatActivity() {
+//Свайпы по картинке
+//http://developer.alexanderklimov.ru/android/views/imageswitcher.php
+
+class ShowpieceActivity : AppCompatActivity(), ViewSwitcher.ViewFactory, GestureDetector.OnGestureListener {
     val viewModel = MuseumViewModel.viewModel
+    private val SWIPE_MIN_DISTANCE = 120
+    private val SWIPE_MAX_OFF_PATH = 250
+    private val SWIPE_THRESHOLD_VELOCITY = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +30,15 @@ class ShowpieceActivity : AppCompatActivity() {
         var showpiece = getShowpiece(intent.getIntExtra(KEY_OBJECT_ID, -1))
 
         supportActionBar?.title = showpiece?.title ?: ""
-        Picasso.get().load(showpiece?.primaryImageSmall).into(ivShowpieces)
+        //Picasso.get().load(showpiece?.primaryImageSmall).into(ivShowpieces)
 
-        //ivShowpieces.setOn
+        imageSwitcher.setFactory(this)
+        val mGestureDetector = GestureDetector(this, this)
+
+        val imageWiew = ImageView(this)
+        Picasso.get().load(showpiece?.primaryImageSmall).into(imageWiew)
+        imageSwitcher.setImageDrawable(imageWiew.drawable)
+
 
         if (showpiece != null) {
             tvDataShowpiece.text = createStringDescription(showpiece)
@@ -115,6 +134,57 @@ class ShowpieceActivity : AppCompatActivity() {
             intent.putExtra(KEY_OBJECT_ID, objectID)
             return  intent
         }
+    }
+
+    override fun makeView(): View {
+        val imageView = ImageView(this)
+        imageView.scaleType = ImageView.ScaleType.FIT_CENTER
+        imageView.layoutParams = FrameLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+        imageView.setBackgroundColor(-0x1000000)
+        return imageView
+
+    }
+
+    override fun onDown(e: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onShowPress(e: MotionEvent?) {
+
+    }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
+        return false
+    }
+
+    override fun onLongPress(e: MotionEvent?) {
+
+    }
+
+    override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+        try {
+            if (Math.abs(e1!!.y - e2!!.y) > SWIPE_MAX_OFF_PATH) return false
+            // справа налево
+            if (e1.x - e2.x > SWIPE_MIN_DISTANCE
+                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                setPositionNext()
+                mImageSwitcher.setImageResource(mImageIds.get(position))
+            } else if (e2.x - e1.x > SWIPE_MIN_DISTANCE
+                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                // слева направо
+                setPositionPrev()
+                mImageSwitcher.setImageResource(mImageIds.get(position))
+            }
+        } catch (e: Exception) {
+            // nothing
+            return true
+        }
+        return true
     }
 
 }
